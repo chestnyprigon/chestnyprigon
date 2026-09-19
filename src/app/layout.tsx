7433685469545de9e0d91a8c40667f03757f2b71
koreakst88 <koreakst88@gmail.com>
2026-09-19 17:50:36 +0900

    feat: add Yandex Metrika tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const manrope = Manrope({ subsets: ["cyrillic", "latin"], variable: "--font-manrope" });
@@ -37,5 +38,25 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
-  return <html lang="ru" className={manrope.variable}><body>{children}</body></html>;
+  return (
+    <html lang="ru" className={manrope.variable}>
+      <body>
+        {children}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`(function(m,e,t,r,i,k,a){
+  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();
+  for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=112810644', 'ym');
+ym(112810644, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});`}
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/112810644" style={{ position: "absolute", left: "-9999px" }} alt="" />
+          </div>
+        </noscript>
+      </body>
+    </html>
+  );
 }
